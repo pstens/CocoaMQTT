71ea4eea967047e48221a611add8f8cf57dcce59
Patrick Stens <patrick.stens@vero.de>
2022-08-22 18:08:52 +0200

    Add fix for "did not call completion handler"
    See https://github.com/emqx/CocoaMQTT/issues/344

--- Source/CocoaMQTTWebSocket.swift
+++ Source/CocoaMQTTWebSocket.swift
@@ -379,6 +379,10 @@ extension CocoaMQTTWebSocket.FoundationConnection: URLSessionWebSocketDelegate {
             self.delegate?.connectionClosed(self, withError: CocoaMQTTError.FoundationConnection.closed(closeCode))
         }
     }
+
+    public func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge) async -> (URLSession.AuthChallengeDisposition, URLCredential?) {
+        return (.performDefaultHandling, nil)
+    }
 }
 
 // MARK: - CocoaMQTTWebSocket.StarscreamConnection
